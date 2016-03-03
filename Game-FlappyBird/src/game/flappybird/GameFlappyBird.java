7e86ea826a835e5d38e1eaadfaeeef860bf3bf31
Jake Young <jake1995young@gmail.com>
2016-03-03 14:40:35 +0000

    Flappy Bird file

---
+++ Game-FlappyBird/src/game/flappybird/GameFlappyBird.java
@@ -0,0 +1,13 @@
+
+package game.flappybird;
+
+public class GameFlappyBird 
+{
+    public final static int width=800;
+    public final static int height=500;
+    private String gameName="Flappy Bird";
+    
+
+
+    
+}
