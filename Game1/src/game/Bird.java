07d0d0456582e65574e4df176564e10517ee2e7e
Jake Young <jake1995young@gmail.com>
2016-03-04 23:29:49 +0000

    added sprite class

--- Game1/src/game/Bird.java
+++ Game1/src/game/Bird.java
@@ -1,6 +1,8 @@
 package game;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class Bird implements Updatable, Renderable
 {
@@ -9,6 +11,23 @@ public class Bird implements Updatable, Renderable
     //negative as in java the y value goes in the opposite direction
     private float baseYVel= -0.6f;
     private float gravity = 0.25f;
+    
+    private Pipes pipes;
+    private int scoredPipe=0;
+    private int score=0;
+    //I created a font to display the score on screen
+    private Font gameFont = new Font("Ariel", Font.BOLD, 30);
+    
+    private BufferedImage flapUp;
+    private BufferedImage flapDown;
+    
+    public Bird (Pipes pipes)
+    {
+        resetBird();
+        
+        this.pipes = pipes;
+    }
+    
     
     public void resetBird()
     {
--- Game1/src/game/Input.java
+++ Game1/src/game/Input.java
@@ -43,7 +43,7 @@ public class Input implements KeyListener
         
     }
 
-
+ 
     
 
 
