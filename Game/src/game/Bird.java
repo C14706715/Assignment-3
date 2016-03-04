91cdf6615d0885b8e67e09f2bb0c18688bc1fc63
Jake Young <jake1995young@gmail.com>
2016-03-04 15:00:26 +0000

    Added Bird class with factors initialised such as gravity and velocity which needed to be taken into account

--- Game/src/game/Bird.java
+++ Game/src/game/Bird.java
@@ -4,11 +4,31 @@ import java.awt.Graphics2D;
 
 public class Bird implements Updatable, Renderable
 {
+    private float x, y;
+    private float yVel;
+    //negative as in java the y value goes in the opposite direction
+    private float baseYVel= -0.6f;
+    private float gravity = 0.25f;
+    
+    public void resetBird()
+    {
+        x=100;
+        y=100;
+        yVel=baseYVel;
+    }
+    
+    public void lap()
+    {
+        yVel = baseYVel;
+        
+    }
+    
+    //This updates the bird
      public void update(Input input)
      {
          
      }
-     
+     //This renders the bird
      public void render(Graphics2D g, float interpolation)
      {
      
