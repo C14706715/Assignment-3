67d9404d8386754b9dbce9cba1db5b1278a6423b
Jake Young <jake1995young@gmail.com>
2016-03-06 16:41:54 +0000

    fixed up the renderable and game classes and added comments where neccessary

--- Game1/src/game/Main.java
+++ Game1/src/game/Main.java
@@ -1,20 +1,24 @@
 package game;
 
 
-
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         Game g = new Game();
-        
+        //Initailise game objects
         Pipes p = new Pipes();
         Bird b = new Bird(p);
         
+        //Add renderables and updatables for pipes
         g.addRenderable(p);
         g.addUpdatable(p);
         
+        //Add renderables and updatables Bird
         g.addRenderable(b);
         g.addUpdatable(b);
         
+        //Start the game
         g.start();
     }
 }
--- Game1/src/game/Renderable.java
+++ Game1/src/game/Renderable.java
@@ -1,8 +1,11 @@
 package game;
 
+//This library gives you extra control on your design of 
+//your interface like color, size etc
 import java.awt.Graphics2D;
 
-public interface Renderable {
+public interface Renderable
+{
     
     public void render(Graphics2D g, float interpolation);
     
